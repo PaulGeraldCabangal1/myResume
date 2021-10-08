fa981dafb1022aaf58323f1b33e2a1266dec97c5
paulgeraldcabangal1 <paulgerald.cabangal@evsu.edu.ph>
2021-10-08 12:21:58 -0500

    first commit

---
+++ myResume/jquery/js.js
@@ -0,0 +1,14 @@
+function myfunction() {
+	document.getElementById("demo").innerHTML = "<strong>Bachelor of Science and Information Technology</strong>";
+};
+function myFunction() {
+	document.getElementById("demo1").innerHTML = "<strong>Eastern Visayas State University</strong>";
+};
+function myffunction() {
+	document.getElementById("demo2").innerHTML = "<strong>09-361-211-721</strong>";
+};
+function myFfunction() {
+	document.getElementById("demo3").innerHTML = "<strong>paulgerald.cabangal@evsu.edu.ph</strong>";
+};
+
+
